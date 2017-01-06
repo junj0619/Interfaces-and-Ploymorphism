7af89a6f9afaac721532645be6db1fea0714b35f
Jun <Jun@JUN-PC>
2017-01-05 22:17:54 -0500

    Add project files.

---
+++ Interfaces and Ploymorphism/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces_and_Ploymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
